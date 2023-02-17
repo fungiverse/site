ebe7841e5377d2e60e0c24fb306a07cfb8ff67d4
TimPietrusky <timpietrusky@gmail.com>
2023-02-17 09:07:41 +0100

    refactor(app): got rid of Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,5 @@ import '@/styles/globals.css';
 import Layout from '@/components/layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
